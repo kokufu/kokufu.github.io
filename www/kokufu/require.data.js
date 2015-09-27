8136226d7e715edd563350504685d2ee7d829068
Yusuke <kokufu.ym@gmail.com>
2015-09-27 10:46:49 +0900

    Add RequireJS

---
+++ www/kokufu/require.data.js
@@ -0,0 +1,10 @@
+requirejs.config({
+    paths: {
+        "Amazon": "http://wms-fe.amazon-adsystem.com/20070822/JP/js/link-enhancer-common.js?tag=team00-22"
+    }
+});
+
+// require(["module/name", ...], function(params){ ... });
+require(["Amazon"], function ($) {
+  // Do nothing
+});
